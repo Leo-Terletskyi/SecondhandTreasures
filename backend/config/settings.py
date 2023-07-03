80a3f20db4dd074d1227df1ac53206487bd6d663
Leonid Terletskyi <leo.terletskyi@gmail.com>
2023-07-03 19:15:58 +0300

    created app products, models of Category and Product

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -52,7 +52,7 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-
+    'products',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
@@ -139,6 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ backend/products/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.template.defaultfilters import slugify
+
+from versatileimagefield.fields import VersatileImageField, PPOIField
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64, unique=True)
+    slug = models.SlugField(blank=True, null=True)
+    
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
+    
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Category, self).save()
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = VersatileImageField(upload_to='images/', ppoi_field='image_ppoi')
+    image_ppoi = PPOIField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['-updated_at']
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return f'/{self.category.slug}/{self.slug}/'
+    
+    def save(self, force_insert=False, force_update=False, using=None, update_fields=None):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Product, self).save()
