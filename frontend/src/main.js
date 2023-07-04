0c14a5417ce244aacd7d73c1acc0bdc893fa4de7
Leonid Terletskyi <leo.terletskyi@gmail.com>
2023-07-04 17:10:57 +0300

    backend: fixed problem with images(wrong wersion of Pillow). frontend: added Home page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,9 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from "axios"
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
 
 /* import the fontawesome core */
 import {library} from '@fortawesome/fontawesome-svg-core'
@@ -19,6 +22,6 @@ library.add(faCartShopping, faUser)
 
 const secondhandTreasures = createApp(App)
 secondhandTreasures.use(store)
-secondhandTreasures.use(router)
+secondhandTreasures.use(router, axios)
 secondhandTreasures.component('font-awesome-icon', FontAwesomeIcon)
 secondhandTreasures.mount('#app')
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,15 +1,112 @@
 <template>
-  <div class="home">
-    Home
+  <div class="homepage">
+    <div class="homepage__head box has-background-primary-light">
+      <h2 class="title is-2">Welcome to our <span style="color: gold">Magazine</span></h2>
+      <h4 class="title is-4">Latest products:</h4>
+    </div>
+    <div class="homepage__body">
+      <div class="columns is-multiline is-desktop">
+        <div class="column is-2" v-for="product in latestProducts" :key="product.id">
+          <div class="card p-1">
+            <div class="card-image">
+              <figure class="image is-3by4">
+                <img :src="product.image.medium_square_crop" alt="">
+              </figure>
+            </div>
+            <div class="card-content p-3">
+              <div class="media">
+                <div class="media-content">
+                  <p class="title is-4">{{ product.name }}</p>
+                  <p class="subtitle is-6">{{ product.category_name }}</p>
+                </div>
+              </div>
+
+              <div class="content">
+                <div class="product-description" v-if="product.description">
+                  <hr>
+                  <p>{{ product.description }}</p>
+                </div>
+                <div class="product-description" v-else>
+                  <hr>
+                  <p>...</p>
+                </div>
+              </div>
+            </div>
+            <div class="card-footer">
+              <div class="field">
+                <div class="control is-expanded pt-2"><strong><span style="color: #42b983">$</span> {{ product.price }}</strong></div>
+                <div class="control">
+                  <router-link :to="product.get_absolute_url">
+                    <button class="button is-primary">Detail..</button>
+                  </router-link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios"
 
 export default {
-  name: 'HomeView',
-  components: {
-
+  name: 'Home',
+  data() {
+    return {
+      latestProducts: [],
+      allCategories: [],
+    }
+  },
+  mounted() {
+    this.getLatestProducts()
+    this.getAllCategories()
+  },
+  methods: {
+    async getLatestProducts() {
+      document.title = 'Home page | Magazine'
+      await axios
+          .get('/api/v1/latest-products')
+          .then(response => {
+            this.latestProducts = response.data
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      this.$store.commit('setIsLoading', false)
+    },
+    getAllCategories() {
+      axios
+          .get('/api/v1/all-categories')
+          .then(response => {
+            this.allCategories = response.data
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.field {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+
+}
+
+.card-footer {
+  display: block;
+  margin-top: auto;
+}
+</style>
