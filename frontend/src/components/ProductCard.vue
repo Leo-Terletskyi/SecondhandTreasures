0d39b88cef7a9818dd139c0a6717e1bfea252ff6
Leonid Terletskyi <leo.terletskyi@gmail.com>
2023-07-07 16:02:25 +0300

    refactoring: moved product card to component

---
+++ frontend/src/components/ProductCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="column is-2">
+    <div class="card p-1">
+      <div class="card-image">
+        <figure class="image is-3by4">
+          <img :src="product.image.medium_square_crop" alt="">
+        </figure>
+      </div>
+      <div class="card-content p-3">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">{{ product.name }}</p>
+            <p class="subtitle is-6">{{ product.category_name }}</p>
+          </div>
+        </div>
+
+        <div class="content">
+          <div class="product-description" v-if="product.description">
+            <hr>
+            <p>{{ product.description }}</p>
+          </div>
+          <div class="product-description" v-else>
+            <hr>
+            <p>...</p>
+          </div>
+        </div>
+      </div>
+      <div class="card-footer">
+        <div class="field">
+          <div class="control is-expanded pt-2"><strong><span style="color: #42b983">$</span> {{ product.price }}</strong></div>
+          <div class="control">
+            <router-link :to="product.get_absolute_url">
+              <button class="button is-primary">Detail..</button>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "ProductCard",
+  props: {
+    product: Object
+  }
+}
+</script>
+
+<style scoped>
+.field {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+.card {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+
+}
+
+.card-footer {
+  display: block;
+  margin-top: auto;
+}
+</style>
--- frontend/src/views/CategoryPage.vue
+++ frontend/src/views/CategoryPage.vue
@@ -6,44 +6,11 @@
     </div>
     <div class="page__body">
       <div class="columns is-multiline is-desktop">
-        <div class="column is-2" v-for="product in products" :key="product.id">
-          <div class="card p-1">
-            <div class="card-image">
-              <figure class="image is-3by4">
-                <img :src="product.image.medium_square_crop" alt="">
-              </figure>
-            </div>
-            <div class="card-content p-3">
-              <div class="media">
-                <div class="media-content">
-                  <p class="title is-4">{{ product.name }}</p>
-                  <p class="subtitle is-6">{{ product.category_name }}</p>
-                </div>
-              </div>
-
-              <div class="content">
-                <div class="product-description" v-if="product.description">
-                  <hr>
-                  <p>{{ product.description }}</p>
-                </div>
-                <div class="product-description" v-else>
-                  <hr>
-                  <p>...</p>
-                </div>
-              </div>
-            </div>
-            <div class="card-footer">
-              <div class="field">
-                <div class="control is-expanded pt-2"><strong><span style="color: #42b983">$</span> {{ product.price }}</strong></div>
-                <div class="control">
-                  <router-link :to="product.get_absolute_url">
-                    <button class="button is-primary">Detail..</button>
-                  </router-link>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <ProductCard
+          v-for="product in products"
+          :key="product.id"
+          :product="product"
+        />
       </div>
     </div>
   </div>
@@ -53,8 +20,13 @@
 import axios from "axios";
 import {toast} from "bulma-toast";
 
+import ProductCard from "@/components/ProductCard";
+
 export default {
   name: "CategoryPage",
+  components: {
+    ProductCard
+  },
   data() {
     return {
       category: '',
@@ -99,21 +71,4 @@ export default {
 </script>
 
 <style scoped>
-.field {
-  display: flex;
-  justify-content: space-between;
-  flex-wrap: wrap;
-}
-
-.card {
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-
-}
-
-.card-footer {
-  display: block;
-  margin-top: auto;
-}
 </style>
